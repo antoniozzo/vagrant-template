a9233f465e0b9702288774cf435859a98c776299
npahlfer <niclas@pahlfer.com>
2015-04-17 17:40:56 +0200

    Added stability;10D

--- provision/app.sh
+++ provision/app.sh
@@ -8,7 +8,7 @@ cd $2
 if [[ $1 == "gulp" ]]; then
 	mkdir -p assets/src/less assets/src/js assets/src/css assets/dist; touch assets/src/less/main.less; touch assets/src/js/main.js; cd assets
 	git clone https://github.com/antoniozzo/coding-with-love.git tmp; rm -rf tmp/gulp/README.md ;mv tmp/gulp/* .; rm -rf tmp
-	npm install -g gulp
+	sudo npm install -g gulp
 	npm install
 fi
 
--- provision/node.sh
+++ provision/node.sh
@@ -3,3 +3,5 @@
 echo "Installing node.."
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
+sudo npm install -g n
+sudo npm install -g npm
